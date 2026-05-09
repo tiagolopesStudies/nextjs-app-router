8d8f10735b50f8696a6b77e6df8272ed1cd55822
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-05-09 19:21:51 -0300

    feat: criação do styleguide e começando o desenvolvimento da página inicial

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="max-w-405 w-full mx-auto p-10 flex flex-col gap-8 h-dvh">
+      <div />
+
+      <main className="grid grid-cols-4 gap-5 flex-1 items-stretch">
+        <div className="bg-navy-800 rounded-xl border-[0.5px] border-navy-500 pt-3 flex flex-col gap-1"></div>
+      </main>
+    </div>
+  )
 }
