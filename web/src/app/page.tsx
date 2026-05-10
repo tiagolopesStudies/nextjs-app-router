227cb39872fde00d131b8e4fd621e59a0306c4c2
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-05-10 17:35:11 -0300

    feat: criação de componente de Section utilizando o padrão Composition

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,28 @@
+import { ArchiveIcon } from 'lucide-react'
+import { Section } from '@/components/section'
+
 export default function Home() {
   return (
     <div className="max-w-405 w-full mx-auto p-10 flex flex-col gap-8 h-dvh">
       <div />
 
       <main className="grid grid-cols-4 gap-5 flex-1 items-stretch">
-        <div className="bg-navy-800 rounded-xl border-[0.5px] border-navy-500 pt-3 flex flex-col gap-1"></div>
+        <Section.Root>
+          <Section.Header>
+            <Section.Title>
+              <ArchiveIcon className="size-3" />
+              Backlog
+            </Section.Title>
+
+            <Section.IssueCount>32</Section.IssueCount>
+          </Section.Header>
+
+          <Section.Content>
+            <div>Card 1</div>
+            <div>Card 2</div>
+            <div>Card 3</div>
+          </Section.Content>
+        </Section.Root>
       </main>
     </div>
   )
