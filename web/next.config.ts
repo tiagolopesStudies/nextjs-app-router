a54c6d6f7a36dd3a944e1154cab9b636a22618f4
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-05-11 16:49:54 -0300

    feat: implementando o login e logout via Github utilizando o Better Auth

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true
+  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com'
+      }
+    ]
+  }
 }
 
 export default nextConfig
--- web/src/app/(board)/header.tsx
+++ web/src/app/(board)/header.tsx
@@ -1,16 +1,30 @@
 'use client'
 
-import { LogInIcon, SearchIcon } from 'lucide-react'
+import { Loader2Icon, LogInIcon, SearchIcon, UserIcon } from 'lucide-react'
+import Image from 'next/image'
 import { debounce, parseAsString, useQueryState } from 'nuqs'
 import { Input } from '@/components/input'
+import { authClient } from '@/lib/auth-client'
 
 export function Header() {
+  const { data: session, isPending } = authClient.useSession()
   const [search, setSearch] = useQueryState('q', parseAsString.withDefault(''))
 
   function handleChangeSearch(event: React.ChangeEvent<HTMLInputElement>) {
     setSearch(event.target.value, {
       limitUrlUpdates: event.target.value.trim() !== '' ? debounce(500) : undefined
     })
+  }
+
+  async function handleSignIn() {
+    await authClient.signIn.social({
+      provider: 'github',
+      callbackURL: `${window.location.href}/`
+    })
+  }
+
+  async function handleLogout() {
+    await authClient.signOut()
   }
 
   return (
@@ -35,12 +49,38 @@ export function Header() {
           />
         </div>
 
-        <button
-          type="button"
-          className="size-8 cursor-pointer rounded-full bg-navy-700 border border-navy-500 flex items-center justify-center hover:bg-navy-600 transition-colors duration-150"
-        >
-          <LogInIcon className="size-3.5 text-navy-200" />
-        </button>
+        {isPending ? (
+          <div className="size-8 rounded-full cursor-pointer bg-navy-700 border-navy-500 flex items-center justify-center">
+            <Loader2Icon className="size-3.5 text-navy-200 animate-spin" />
+          </div>
+        ) : session?.user ? (
+          <button
+            type="button"
+            onClick={handleLogout}
+            className="size-8 rounded-full overflow-hidden"
+          >
+            {session.user.image ? (
+              <Image
+                src={session.user.image}
+                alt={session.user.name}
+                width={0}
+                height={0}
+                draggable={false}
+                className="size-8 rounded-full cursor-pointer"
+              />
+            ) : (
+              <UserIcon className="size-3.5 text-navy-200" />
+            )}
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={handleSignIn}
+            className="size-8 cursor-pointer rounded-full bg-navy-700 border border-navy-500 flex items-center justify-center hover:bg-navy-600 transition-colors duration-150"
+          >
+            <LogInIcon className="size-3.5 text-navy-200" />
+          </button>
+        )}
       </div>
     </div>
   )
